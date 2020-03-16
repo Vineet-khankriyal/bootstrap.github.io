b3744faeb435f84b19e9eea83c488a3ebd50fe7a
VINEET KHANKRIYAL <vkkhankriyal@gmail.com>
2020-03-16 13:36:12 +0530

    Publishing my first website
    WoW! I'm very Happy.

---
+++ mywebsite1/about.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link href="https://fonts.googleapis.com/
+  css?family=Josefin+Sans&display=swap">
+  
+</head>
+<body>
+
+<?php include 'menu.php' ?>
+
+<div class="jumbotron">
+  <h1>MRV Enterprise</h1>
+  <p>Vision and Multi Resolution</p>
+</div>
+
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">About Us</h2>
+    </div>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg-6 col-md-6 col-12">
+          <img src="images/about me.jpg" class="img-fluid aboutimg">
+          </div>
+        <div class="col-lg-6 col-md-6 col-12">
+        <h2 class="display-4">We creates a perfect solution of "DESIGNING | PRINTING | BRANDING"</h2>
+        <p class="py-3">We only 'designes' and our designes creates a new world of feeling, colors, etc. </p>
+        <a href="about.php" class="btn btn-outline-success"> Check More </a>
+      </div>  
+    </div>
+  </div>  
+</section>
+<footer>
+  <p class="p-3 bg-dark text-center text-white">@MRV ENTERPRISE</p>
+</footer>
+
+
+</body>
+
+</html>
---
+++ mywebsite1/index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link href="https://fonts.googleapis.com/
+  css?family=Josefin+Sans&display=swap">
+  
+</head>
+<body>
+
+<?php include 'menu.php' ?>
+
+<div class="jumbotron">
+  <h1>MRV Enterprise</h1>
+  <p>Vision and Multi Resolution</p>
+</div>
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/los angeles.jpg" alt="Los Angeles" width="1100" height="500">
+    </div>
+    <div class="carousel-item">
+      <img src="images/chicago.jpg" alt="Chicago" width="1100" height="500">
+    </div>
+    <div class="carousel-item">
+      <img src="images/new york city.jpg" alt="New York" width="1100" height="500">
+    </div>
+  </div>
+  
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center">About Us</h2>
+		</div>
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-lg-6 col-md-6 col-12">
+					<img src="images/about me.jpg" class="img-fluid aboutimg">
+					</div>
+				<div class="col-lg-6 col-md-6 col-12">
+				<h2 class="display-4">We creates a perfect solution of "DESIGNING | PRINTING | BRANDING"</h2>
+				<p class="py-3">We only 'designes' and our designes creates a new world of feeling, colors, etc. </p>
+				<a href="about.php" class="btn btn-outline-success"> Check More </a>
+			</div>	
+		</div>
+	</div>	
+</section>
+
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center">Our Services</h2>
+	</div>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-4 col-md-4 col-12">
+				<div class="card">
+  					<img class="card-img-top" src="images/graphic-design" alt="Card image">
+  					<div class="card-body">
+    					<h4 class="card-title">Complete Designing Solution:)</h4>
+    					<p class="card-text">We have a complete solution for designing.</p>
+    					<a href="#" class="btn btn-outline-success">See Profile</a>
+  					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<div class="card">
+  					<img class="card-img-top" src="images/graphic-design" alt="Card image">
+  					<div class="card-body">
+    					<h4 class="card-title">Complete Printing Solution:)</h4>
+    					<p class="card-text">We have complete solutions for printing also.</p>
+    					<a href="#" class="btn btn-outline-success">See Profile</a>
+  					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<div class="card">
+  					<img class="card-img-top" src="images/graphic-design" alt="Card image">
+  					<div class="card-body">
+    					<h4 class="card-title">Social Media Optimisation:)</h4>
+    					<p class="card-text">We also works for social media optimiasation.</p>
+    					<a href="#" class="btn btn-outline-success">See Profile</a>
+  					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center">Gallary</h2>
+	</div>
+
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-3 col-md-3 col-12">
+				<img src="images/wallpaper.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-3 col-md-3 col-12">
+				<img src="images/wallpaper.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-3 col-md-3 col-12">
+				<img src="images/wallpaper.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-3 col-md-3 col-12">
+				<img src="images/wallpaper.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-3 col-md-3 col-12">
+				<img src="images/wallpaper.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-3 col-md-3 col-12">
+				<img src="images/wallpaper.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-3 col-md-3 col-12">
+				<img src="images/wallpaper.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-3 col-md-3 col-12">
+				<img src="images/wallpaper.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-3 col-md-3 col-12">
+				<img src="images/wallpaper.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-3 col-md-3 col-12">
+				<img src="images/wallpaper.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-3 col-md-3 col-12">
+				<img src="images/wallpaper.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-3 col-md-3 col-12">
+				<img src="images/wallpaper.jpg" class="img-fluid pb-4">
+			</div>
+		</div>
+	</div>
+
+</section>
+
+
+
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center">Join Us</h2>
+	</div>
+
+	<div class="w-50 m-auto">
+		<form action="userinfo.php" method="post">
+			<div class="form-group">
+				<label>Username</label>
+				<input type="text" name="user" autocomplete="off" class="form-control">
+			</div>
+			<div class="form-group">
+				<label>Email Id</label>
+				<input type="text" name="email" autocomplete="off" class="form-control">
+				</div>
+				<div class="form-group">
+				<label>Mobile</label>
+				<input type="text" name="mobile" autocomplete="off" class="form-control">
+			</div>
+				<div class="form-group">
+				<label>Comments</label>
+				<textarea name="comments" class="form-control">
+				</textarea>
+			</div>
+			 <button type="submit" class="btn btn-outline-success">Submit
+			 </button>
+		</form>
+	</div>
+</section>
+
+<footer>
+	<p class="p-3 bg-dark text-center text-white">@MRV ENTERPRISE</p>
+</footer>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+</body>
+</html>
